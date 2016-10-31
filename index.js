71e816be2dde07ba3d7596e69f30e58dc8fe49c5
davidg <davidg@maxx>
2016-10-31 15:35:20 -0400

    copied index.js sample.js in node modules

--- index.js
+++ index.js
@@ -29,56 +29,34 @@ if (token) {
   require('beepboop-botkit').start(controller, { debug: true })
 }
 
-var wit = Witbot(witToken)
+var witbot = Witbot(witToken)
 
-controller.on('bot_channel_join', function (bot, message) {
-  bot.reply(message, "I'm here!")
-})
+controller.hears('.*', 'direct_message,direct_mention', function (bot, message) {
+    var wit = witbot.process(message.text, bot, message)
 
-wit.hears('hello', 0.5, function(bot, message, outcome) {
-  bot.reply(message, 'what it is')
-})
-// controller.hears(['hello', 'hi'], ['direct_mention'], function (bot, message) {
-//   bot.reply(message, 'Hello.')
-// })
-//
-// controller.hears(['hello', 'hi'], ['direct_message'], function (bot, message) {
-//   bot.reply(message, 'Hello.')
-//   bot.reply(message, 'It\'s nice to talk to you directly.')
-// })
-//
-// controller.hears('.*', ['mention'], function (bot, message) {
-//   bot.reply(message, 'You really do care about me. :heart:')
-// })
+    wit.hears('hello', 0.53, function (bot, message, outcome) {
+        bot.startConversation(message, function (_, convo) {
+            convo.say('Hello!')
+            convo.ask('How are you?', function (response, convo) {
+                witbot.process(response.text)
+                    .hears('good', 0.5, function (outcome) {
+                        convo.say('I am so glad to hear it!')
+                        convo.next()
+                    })
+                    .hears('bad', 0.5, function (outcome) {
+                        convo.say('I\'m sorry, that is terrible')
+                        convo.next()
+                    })
+                    .otherwise(function (outcome) {
+                        convo.say('I\'m cofused')
+                        convo.repeat()
+                        convo.next()
+                    })
+            })
+        })
+    })
 
-controller.hears('help', ['direct_message', 'direct_mention'], function (bot, message) {
-  var help = 'I will respond to the following messages: \n' +
-      '`bot hi` for a simple message.\n' +
-      '`bot attachment` to see a Slack attachment message.\n' +
-      '`@<your bot\'s name>` to demonstrate detecting a mention.\n' +
-      '`bot help` to see this again.'
-  bot.reply(message, help)
-})
-
-controller.hears(['attachment'], ['direct_message', 'direct_mention'], function (bot, message) {
-  var text = 'Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots.'
-  var attachments = [{
-    fallback: text,
-    pretext: 'We bring bots to life. :sunglasses: :thumbsup:',
-    title: 'Host, deploy and share your bot in seconds.',
-    image_url: 'https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png',
-    title_link: 'https://beepboophq.com/',
-    text: text,
-    color: '#7CD197'
-  }]
-
-  bot.reply(message, {
-    attachments: attachments
-  }, function (err, resp) {
-    console.log(err, resp)
-  })
-})
-
-controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
-  bot.reply(message, 'Sorry <@' + message.user + '>, I don\'t understand. \n')
+    wit.otherwise(function (bot, message) {
+        bot.reply(message, 'You are so intelligent, and I am so simple. I don\'t understnd')
+    })
 })
