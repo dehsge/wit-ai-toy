79f0fd133d37b4cd25f956f42820d3420fe46ac1
davidg <davidg@maxx>
2016-10-31 15:13:03 -0400

    switched to witbot due to parse err due to node v4.2.6 being used instead of 6+.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var Botkit = require('botkit')
 var wit = require('node-wit')
 
 var token = process.env.SLACK_TOKEN
+var witTokent = process.env.WIT_TOKEN
 
 var controller = Botkit .slackbot({
   // reconnect to Slack RTM when connection goes bad
