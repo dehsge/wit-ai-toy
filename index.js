cbbf0e9329111f960ed3dd30e1a95256759ef353
davidg <davidg@maxx>
2016-11-01 09:42:27 -0400

    removed old witbot code

--- index.js
+++ index.js
@@ -28,35 +28,3 @@ if (token) {
   console.log('Starting in Beep Boop multi-team mode')
   require('beepboop-botkit').start(controller, { debug: true })
 }
-
-var witbot = Witbot(witToken)
-
-controller.hears('.*', 'direct_message,direct_mention', function (bot, message) {
-    var wit = witbot.process(message.text, bot, message)
-
-    wit.hears('hello', 0.53, function (bot, message, outcome) {
-        bot.startConversation(message, function (_, convo) {
-            convo.say('Hello!')
-            convo.ask('How are you?', function (response, convo) {
-                witbot.process(response.text)
-                    .hears('good', 0.5, function (outcome) {
-                        convo.say('I am so glad to hear it!')
-                        convo.next()
-                    })
-                    .hears('bad', 0.5, function (outcome) {
-                        convo.say('I\'m sorry, that is terrible')
-                        convo.next()
-                    })
-                    .otherwise(function (outcome) {
-                        convo.say('I\'m cofused')
-                        convo.repeat()
-                        convo.next()
-                    })
-            })
-        })
-    })
-
-    wit.otherwise(function (bot, message) {
-        bot.reply(message, 'You are so intelligent, and I am so simple. I don\'t understnd')
-    })
-})
