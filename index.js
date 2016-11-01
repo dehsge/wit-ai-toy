6496a9b4a7774f5c6a3f3db4b8894d01def5b91d
davidg <davidg@maxx>
2016-11-01 15:06:37 -0400

    intent test 2

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ if (token) {
 
 var witbot = Witbot.Wit({accessToken: witToken})
 
-witbot.message('hello hello', {})
+witbot.message('hey', {})
     .then((data) => {
     console.log('Yay, got Wit.ai response: ' + JSON.stringify(data))
 })
